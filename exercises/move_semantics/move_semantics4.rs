31388af5ddeb2168ba2ba58062f4b5d79f908a39
Néstor Arias <narias@publicissapient.com>
2022-09-07 14:48:37 -0500

    reworked some of the exercises of move semantics

--- exercises/move_semantics/move_semantics4.rs
+++ exercises/move_semantics/move_semantics4.rs
@@ -3,23 +3,18 @@
 // the Vector gets created in the function itself and passed back to the main
 // function.
 // Execute `rustlings hint move_semantics4` or use the `hint` watch subcommand for a hint.
-
-// I AM DONE
-
 fn main() {
-    let mut vec1 = fill_vec();
 
+    let mut vec1 = fill_vec();
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
     vec1.push(88);
-
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
 // `fill_vec()` no longer takes `vec: Vec<i32>` as argument
 fn fill_vec() -> Vec<i32> {
-    // create a Vec of i32 and fill it with values 1..=10
-    let mut vec: Vec<i32> = Vec::new();
+    let mut vec = Vec::new();
 
     vec.push(22);
     vec.push(44);
